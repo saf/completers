61e42b338275032e91a436177cd751a78871bb17
Sławek Rudnicki <slawek.rudnicki@gmail.com>
2017-06-10 00:54:17 +0200

    Fs: Recurse into directories to find completions
    We modify the Filesystem completer so that it recurses into
    directories using a breadth-first search algorithm. When the completer
    is pointed at a path, it provides completions coming not only from the
    directory's children, but also their children, etc.
    
    This is a step in preparation for implementing search in the
    Filesystem completer, where we will want to search not only for
    entries directly within the visited directory, but also deeper into
    the directory hierarchy.

--- src/completers/filesystem.rs
+++ src/completers/filesystem.rs
@@ -5,10 +5,13 @@ use std::any;
 use std::fs;
 use std::path;
 use std::rc::Rc;
+use std::collections::vec_deque::VecDeque;
 
 use termion::color;
 
 use core;
+
+const DIRECTORY_DEPTH_LIMIT: usize = 4;
 
 #[derive(PartialEq)]
 enum FsEntryType {
@@ -24,7 +27,7 @@ struct FsCompletion {
 
 impl core::Completion for FsCompletion {
     fn result_string(&self) -> String {
-        self.relative_path.to_str().unwrap().to_string()
+        self.relative_path.to_string_lossy().into_owned()
     }
 
     fn display_string(&self) -> String {
@@ -41,6 +44,12 @@ impl core::Completion for FsCompletion {
     }
 }
 
+/// Type representing an entry in the BFS queue of directory enumeration.
+///
+/// The first element is a directory path, and the second element signifies
+/// the depth of the directory in the search.
+struct DirectoryQueueEntry(path::PathBuf, usize);
+
 pub struct FsCompleter {
     current_path: path::PathBuf,
     completions: core::Completions,
@@ -56,10 +65,18 @@ impl FsCompleter {
         completer
     }
 
-    fn update_completions(&mut self) {
-        let root_path = self.current_path.as_path();
-        let mut completions: core::Completions = Vec::new();
-        let mut entries = fs::read_dir(root_path).unwrap();
+    fn directory_bfs(queue: &mut VecDeque<DirectoryQueueEntry>) -> core::Completions {
+        let queue_entry = queue.pop_front();
+        if let None = queue_entry {
+            return core::Completions::new();
+        }
+        let DirectoryQueueEntry(dir_path, depth) = queue_entry.unwrap();
+        let mut completions = core::Completions::new();
+        let mut read_dir_result = fs::read_dir(&dir_path);
+        if let Err(_) = read_dir_result {
+            return core::Completions::new();
+        }
+        let mut entries = read_dir_result.unwrap();
         while let Some(Ok(entry)) = entries.next() {
             let entry_type = match entry.metadata() {
                 Ok(md) =>
@@ -72,7 +89,7 @@ impl FsCompleter {
             };
 
             let here_prefix = path::Path::new("./");
-            let mut path = root_path.join(entry.file_name());
+            let mut path = dir_path.join(entry.file_name());
             if path.starts_with(here_prefix) {
                 path = path.strip_prefix(here_prefix).unwrap().to_path_buf();
             }
@@ -82,12 +99,26 @@ impl FsCompleter {
                 }
             }
 
+            if entry_type == FsEntryType::Directory && depth < DIRECTORY_DEPTH_LIMIT {
+                queue.push_back(DirectoryQueueEntry(path.clone(), depth + 1));
+            }
+
             completions.push(Rc::new(FsCompletion {
                 relative_path: path,
                 entry_type: entry_type,
             }));
         }
         completions.sort_by_key(|b| b.result_string());
+        completions
+    }
+
+    fn update_completions(&mut self) {
+        let mut dir_queue: VecDeque<DirectoryQueueEntry> = VecDeque::new();
+        dir_queue.push_back(DirectoryQueueEntry(self.current_path.clone(), 0));
+        let mut completions = core::Completions::new();
+        while !dir_queue.is_empty() {
+            completions.extend(FsCompleter::directory_bfs(&mut dir_queue));
+        }
         self.completions = completions;
     }
 }
