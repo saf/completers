ec28c57b0616b8c85212d96119f8c5111b30f999
Sławek Rudnicki <slawek.rudnicki@gmail.com>
2017-06-21 00:13:42 +0200

    init: Allow to specify build type in init.sh
    We modify init.sh to accept the build type ('debug' or 'release') as
    its parameter to allow testing the release version to benchmark
    performance. This is for development purposes only; the script will
    have to be modified to include a more robust path to the completers
    binary before any release can take place.

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,9 @@
 COMPLETERS_DIR=$(dirname $(readlink -f ${BASH_SOURCE}))
 
+target=${1:-debug}
+
 function completers_complete {
-    read point line < <("${COMPLETERS_DIR}/../target/debug/completers" --point="${READLINE_POINT}" "${READLINE_LINE}")
+    read point line < <("${COMPLETERS_DIR}/../target/$target/completers" --point="${READLINE_POINT}" "${READLINE_LINE}")
     READLINE_LINE=$line
     READLINE_POINT=$point
 }
